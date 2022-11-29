2cb42f743af97cd928bb1c95a482daf8f46b92d6
Vitaly Gruver <vitgruver@gmail.com>
2022-11-30 00:30:04 +0300

    First task complete

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-/*
 
+Console.Write("Input first number: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Input second number: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+if (number1 > number2)
+    {
+        Console.WriteLine("max = " + number1);
+    }
+else 
+    {
+        Console.WriteLine("max = " + number2);
+    }
+
+/*
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
-
-*/
+/*
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
