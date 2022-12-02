3cf31cded77424dfa8c3e5eec50dffcf36f43806
Vitaly Gruver <vitgruver@gmail.com>
2022-12-02 20:44:52 +0300

    Add method

--- Program.cs
+++ Program.cs
@@ -1,39 +1,16 @@
-﻿// Семинар 1. 
+﻿int CutNumber(int num)
+{
+    int sot = num / 100;
+    int ed = num % 10;
 
-// Задача 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+    int result = sot * 10 + ed;
+    return result;
+}
 
-/*
+int randNum = new Random().Next(100, 1000);
 
-Console.Write("Input first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+int newNum = CutNumber(randNum);
 
-Console.Write("Input second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"New version of a number {randNum} is {newNum}.");
 
-int quad = num2 * num2;
-
-if(num1 == quad)
-    {
-        Console.WriteLine("Yes!");
-    }
-else
-    {
-        Console.WriteLine("No!");
-    }
-
-*/
-
-// Задача 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
-
-Console.Write("Input an integer number: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-int current = -n;
-
-while(current <= n)
-    {
-        Console.Write(current + " ");
-        current++;
-    }
-
-
+if(newNum > 0 && newNum % 2 == 0)
