761b95f6f84723e83799753d9b27e09557a9efa2
Vitaly Gruver <vitgruver@gmail.com>
2022-12-06 21:30:26 +0300

    add Seminar 2

--- Program.cs
+++ Program.cs
@@ -1,72 +1,98 @@
-﻿// 1. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+﻿/*
 
-/*
-int Max(int maxNum)
-{
-    int num1 = maxNum / 10;
-    int num2 = maxNum % 10;
-    if (num1>num2)
-        return num1;
-    return num2;
-}
+int FindQuadrant(double x, double y)
+    {
+    if(x>0 && y>0) return 1;
+    if(x<0 && y>0) return 2;
+    if(x<0 && y< 0) return 3;
+    if(x>0 && y<0) return 4;
+    return 0;
+    };
 
-int randomNum = new Random().Next(10, 100);
+Console.WriteLine("Input x: ");
+double xA = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine($"Случайное число - {randomNum}. Максимальная цифра - {Max(randomNum)}");
+Console.WriteLine("Input y: ");
+double xA = Convert.ToDouble(Console.ReadLine());
 
-*/
+int quadNum = FindQuadrant(xa,ya);
 
-
-// 2. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
-
-/*
-
-bool Div(int a, int b)
-
-{
-    if (a % b == 0)
-        return true;
-    return false;
-}
-
-Console.WriteLine("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-bool result = Div(num1, num2);
-
-Console.WriteLine(result);
+Console.WriteLine($"Point = {quadNum}");
 
 */
 
 
-// 3. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b.
+// 1. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
 /*
 
-bool Div(int a, int b, int c)
+void Range (int quart)
+    {
+        if (quart == 1)
+        Console.Write("x(0 ... +∞), y(0 ... +∞)");
 
-{
-    if (a % b == 0)
-        if (a % c == 0)
-            return true;
-    return false;
-}
+        if (quart == 2)
+        Console.Write("x(-∞ ... 0), y(0 ... +∞)");
 
+        if (quart == 3)
+        Console.Write("x(-∞ ... -0), y(-∞ ... 0)");
 
-Console.WriteLine("Введите число для проверки: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+        if (quart == 4)
+        Console.Write("x(0 ... +∞), y(-∞ ... 0)");
 
-Console.WriteLine("Введите первый делитель: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+        if (quart < 1 || quart > 4)
+        Console.Write("Incorrect quarter");
+        
+    }
 
-Console.WriteLine("Введите второй делитель: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input quarter: ");
+int quart = Convert.ToInt32(Console.ReadLine());
 
-bool result = Div(num1, num2, num3);
-
-Console.WriteLine(result);
+Range(quart);
 
 */
+
+// 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+/*
+
+double FindDistance(double xA, double yA, double xB, double yB)
+{
+    double distance = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2));
+    return distance;
+}
+
+Console.WriteLine("Input first x: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input first y: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input second x: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input second y: ");
+double yB = Convert.ToDouble(Console.ReadLine());
+
+double dist = Math.Round(FindDistance(xA, yA, xB, yB),3);
+
+Console.WriteLine($"Distance = {dist}");
+
+*/
+
+
+// 3. Напишите программу, которая принимает на вход число (N) и выдаёт ряд квадратов чисел от 1 до N.
+
+void RowSquares(int N)
+{
+    int result = 1;
+
+    while (result <= N)
+    {
+        Console.Write(Math.Pow(result, 2) + " ");
+        result++;
+    }
+}
+Console.WriteLine($"Input N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+RowSquares(N);
+
+
