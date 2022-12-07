e2ee2bf17b752037bb4bf8971f67a814a4e221e1
VitGruver <vitgruver@gmail.com>
2022-12-07 20:06:59 +0300

    Add 1  task to homework 3

--- Program.cs
+++ Program.cs
@@ -1,84 +1,60 @@
-// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 1. Напишите программу, которая принимает на вход число и проверяет, является ли оно палиндромом.
 
-/*
-
-int ShowSecondNum(int num)
+void checkPolindrom(int num)
 {
-    int result = num / 10 % 10;
-    return result;
-}
-
-int randomNum = new Random().Next(100, 1000);
-
-Console.WriteLine($"Вторая цифра числа {randomNum} - {ShowSecondNum(randomNum)}");
-
-*/
-
-
-// 2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-/*
-
-int ShowThirdNum(int num)
-{
-    int thirdNum = -1;
-    if (num < 100)
+    int FindNumOfDigits(int num)    //  Определяем сколько разрядов у числа
     {
-        return thirdNum;
-    }
-    else
-    {
-        if (num > 1000)
+        int divider = 1;
+        int quotient = num / divider;
+        int result = 1;
+
+        while (quotient > 9)
         {
-            while (num > 1000)
-            {
-                num = num / 10;
-            }
+            divider = divider * 10;
+            quotient = num / divider;
+            result++;
         }
-
-        thirdNum = num % 10;
-
-        return thirdNum;
+        return result;
     }
+
+    int step = FindNumOfDigits(num) - 1;
+    int numOnTheLeft = 0;
+    int numOnTheRight = 0;
+
+    while (num > 9 && numOnTheLeft == numOnTheRight)    //  Сравниваем крайние цифры, если равны - отбрасываем их и повторяем
+    {
+        numOnTheLeft = num / Convert.ToInt32(Math.Pow(10, step));
+        numOnTheRight = num % 10;
+
+        num = num % Convert.ToInt32(Math.Pow(10, step)) / 10;
+
+        step = step - 2;
+
+//      Console.WriteLine($"Index = {step} NumberOnTheLeft = {numOnTheLeft} NumberOnTheRight = {numOnTheRight} num = {num}");   // проверка вычислений цикла
+    }
+
+    if (numOnTheLeft == numOnTheRight) Console.WriteLine("Число является палиндромом!");
+    else Console.WriteLine("Число не является палиндромом!");
+
 }
 
-Console.WriteLine("Привет! Введи любое число больше 99 и я покажу тебе его третью цифру: ");
+Console.WriteLine("Введите число для проверки: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int result = ShowThirdNum(num);
+checkPolindrom(num);
 
-if (result == -1)
-    {
-        Console.WriteLine("Ну я же просил больше 99 (");
-    }
-else
-    {
-        Console.WriteLine($"Третья цифра числа {num} - это {result}");
-    }
 
-*/
-
-// 3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
+// 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 /*
+A (3,6,8); B (2,1,-7), -> 15.84
 
-bool CheckDayOff(int dayNum)
-{
-    if (dayNum == 6 || dayNum == 7)
-        return true;
-    else
-        return false;
-}
+A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int day = new Random().Next(1, 8);
+Задача 23
 
-bool dayOff = CheckDayOff(day);
+// 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-if (dayOff == true)
-{
-    Console.WriteLine($"{day} день недели - это выходной!");
-}
-else
-    Console.WriteLine($"{day} день недели - это не выходной!");
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
 
 */
