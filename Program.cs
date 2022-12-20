d5a36d4096436c36fea9ad42c9c7f8d9cdd8cb18
VitGruver <vitgruver@gmail.com>
2022-12-20 16:00:16 +0300

    Add homework. Task 3

--- Program.cs
+++ Program.cs
@@ -1,26 +1,40 @@
-void FillArray(int[] collection)
+/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+*/
+int[] CreateRandomArray(int size)
 {
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        collection[index] = new Random().Next(1, 10);
-        index++;
+        Console.WriteLine($"Введите {i+1} элемент массива: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        array[i] = num;
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
     }
 }
 
-void PrintArray(int[] col)
-{
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
-        Console.Write(col[position] + " ");
-        position++;
-    }
-}
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
 
-int[] array = new int[10];
-
-FillArray(array);
-PrintArray(array);
+ShowArray(CreateRandomArray(size));
