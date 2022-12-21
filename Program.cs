4c7396cb2336f866209fa7a58511be6ef6f6fb68
VitGruver <vitgruver@gmail.com>
2022-12-21 14:00:24 +0300

    Add tasks

--- Program.cs
+++ Program.cs
@@ -1,19 +1,54 @@
-/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-3, 5 -> 243 (3⁵)
+/*
+int Exponentiation (int num, int degree)
+{
+    int result = num;
+    for (int i = 1; i < degree; i++)
+    {
+        result = result * num;
+    }
+    return result;
+}
 
-2, 4 -> 16
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
-Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите степень в которую необходимо возвести число: ");
+int degree = Convert.ToInt32(Console.ReadLine());
 
-452 -> 11
+int result = Exponentiation (num, degree);
 
-82 -> 10
-
-9012 -> 12
-
-Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine($"{num} в степени {degree} - это {result}");
 */
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*
+
+int FindSum(int num)
+{
+    int sum = num % 10;
+    int current = num / 10;
+    while (current > 0)
+    {
+        sum = sum + current % 10;
+        current = current / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum = FindSum(num);
+
+Console.WriteLine($"Сумма цифр в числе равна {sum}");
+/*
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+/*
 int[] CreateRandomArray(int size)
 {
     int[] array = new int[size];
@@ -38,3 +73,4 @@ Console.WriteLine("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 
 ShowArray(CreateRandomArray(size));
+*/
